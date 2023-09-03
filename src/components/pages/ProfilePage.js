c6128913070ca263c5b53d8b5ccb0850a5ca4c51
vanshika308 <vanshikatiwari0308@gmail.com>
2023-09-04 00:00:31 +0530

    verify user email

--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,29 +1,72 @@
-
+import React, { useContext, useState } from "react";
+import AuthContext from '../../store/AuthContext';
 import ProfileUpdate from './ProfileUpdate';
 import './Styles/ProfilePage.css';
-import { useState } from 'react';
 
-const ProfilePage=()=>{
+const ProfilePage = () => {
+   const [update, setUpdate] = useState(false);
+   const [verificationSent, setVerificationSent] = useState(false); 
+   const authContext = useContext(AuthContext);
 
-   const[update,setUpdate]=useState(false);
-
-   const handleUpdateClick =()=>{
+   const handleUpdateClick = () => {
       setUpdate(true);
-   }
+   };
 
-   return(
+   const sendVerificationEmail = () => {
+      const idToken = authContext.token;
+
+      if (idToken) {
+         const requestBody = {
+            requestType: "VERIFY_EMAIL",
+            idToken: idToken,
+         };
+
+         fetch(
+            `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDuGcgdMdoEb10Z2SKOOb8vttuUJsRYfDg`, // Replace [API_KEY] with your Firebase project's Web API Key
+            {
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+               body: JSON.stringify(requestBody),
+            }
+         )
+            .then((response) => {
+               if (response.ok) {
+                  setVerificationSent(true); 
+                  console.log("Email verification sent.");
+               } else {
+                  throw new Error("Error sending email verification.");
+               }
+            })
+            .catch((error) => {
+               console.error("Error:", error);
+            });
+      }
+   };
+
+   return (
       <div className="container">
-      <h1 className="heading">WELCOME TO EXPENSE TRACKER</h1>
-      {!update && (
-        <div className="incomplete-profile">
-          <p>Your Profile is Incomplete</p>
-          <button onClick={handleUpdateClick}>Complete now</button>
-        </div>
-      )}
-      {update && <ProfileUpdate />}
-      <hr />
-    </div>
+         <h1 className="heading">WELCOME TO EXPENSE TRACKER</h1>
+         {!update && (
+            <div className="incomplete-profile">
+               <p>Your Profile is Incomplete</p>
+               <button onClick={handleUpdateClick}>Complete now</button>
+            </div>
+         )}
+         <hr />
+         {update && <ProfileUpdate />}
+         <hr />
+         {!verificationSent && (
+            <div className="verification">
+               <h5>Verify Your Email</h5>
+               <button onClick={sendVerificationEmail}>
+                  {verificationSent ? 'Resend Verification' : 'Send Verification'}
+               </button>
+            </div>
+         )}
+      </div>
    );
-}
+};
 
 export default ProfilePage;
