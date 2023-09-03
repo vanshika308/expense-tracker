2f5643fdc317dfa067ce4c06f233ea10ddfc05e6
vanshika308 <vanshikatiwari0308@gmail.com>
2023-09-04 00:14:05 +0530

    Logout User

--- src/store/AuthProvider.js
+++ src/store/AuthProvider.js
@@ -3,18 +3,24 @@ import AuthContext from "./AuthContext";
 
 const AuthProvider= (props)=>{
 
-    const [email, setEmail] =useState();
-    const [token, setToken] =useState();
+    const storedToken = localStorage.getItem("token");
+    const storedEmail = localStorage.getItem("email");
+
+    const [email, setEmail] =useState(storedEmail);
+    const [token, setToken] =useState(storedToken);
 
     const loginHandler=(token,email)=>{
-        
         setEmail(email);
         setToken(token);
+        localStorage.setItem('token',token);
+        localStorage.setItem('email',email);
     }
 
     const logoutHandler=()=>{
         setEmail("");
         setToken("");
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
     }
   
     const userIsLoggedIn= !! token;
